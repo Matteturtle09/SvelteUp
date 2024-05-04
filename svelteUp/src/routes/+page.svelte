707388e9cd3c2f108a4ff9b40a56f0eb5ca1a257
Matteturtle09 <mauri.matteo16@gmail.com>
2024-05-04 09:19:01 +0000

    base app stack

---
+++ svelteUp/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Navbar from "../lib/components/navbar.svelte";
+  import Hero from "../lib/components/hero.svelte";
+</script>
+<Navbar></Navbar>
+<Hero imageSrc="https://images.unsplash.com/photo-1503756234508-e32369269deb?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" mainTitle="Hello word" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolores aliquid officiis repudiandae odio natus, adipisci autem placeat incidunt animi ad. Magnam aliquam voluptatem nihil expedita quisquam ipsam, in ratione?"></Hero>
